d9e76fcea841001538633d81532d32e244755e58
Richard Myatt <lydgate77@hotmail.co.uk>
2019-03-29 21:59:32 +0000

    All modification to demo keyboard done - upload test to be performed

--- js/keyboard.js
+++ js/keyboard.js
@@ -1,7 +1,13 @@
-var notes, keys, whiteKeys, blackKeys, osc;
-var selection;
-var audioContext, osc;
+// establish variables
+var notes, whiteKeys, blackKeys;
+var allKeys;
+var audioContext, osc, oscillators;
+
+// data
 var playing = false;
+var oscillators = ["sine", "square", "sawtooth", "triangle"];
+var oscType = "sine";
+var oscIndex = 0;
 
 notes = {
   "C4"   : 261.626,           // magSq()iddle C
@@ -22,18 +28,68 @@ notes = {
 whiteKeys = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
 blackKeys = ["Csh4", "Dsh4", "Fsh4", "Gsh4", "Ash4"];
 
-var s = Snap("#keyboard8");
+// obtaine a reference to the svg in the dom
+var s = Snap("#oscKeyboard");
 
-// obtain a reference to all keys
-selection = s.selectAll("use");
+// add a background -->
+var bg = s.rect(0, 0, "100%", "100%", 5, 5);
+bg.attr({
+  fill: "#333"
+});
 
-// assign id's to all keys
-for(var i = 0; i < whiteKeys.length; i++) {
-  selection[i].attr({id: whiteKeys[i]});
-}
+// add a button to select the oscillator
+var oscBtn = s.rect(10, 15, 60, 20, 3, 3);
+oscBtn.attr({
+  fill: "blue"
+});
 
-for(i = whiteKeys.length; i < selection.length; i++) {
-  selection[i].attr({id: blackKeys[i - whiteKeys.length]});
+
+
+var btnText = s.text(40, 28, oscType);
+btnText.attr({
+  fill: "yellow",
+  "font-size": 10,
+  "text-anchor": "middle"
+});
+
+var oscText = s.text(80, 28, "- oscillator type");
+oscText.attr({
+  fill: "#fff",
+  "font-size": 10
+});
+
+var btnCover = s.rect(10, 15, 60, 20, 3, 3);
+btnCover.attr({
+  "fill-opacity": 0
+});
+
+
+// add a decorative red line
+var decLine = s.line(11, 54, 210, 53);
+decLine.attr({
+  stroke: "red",
+  "stroke-width": 3
+});
+
+// create a group to load our external svg
+var keyboardGroup = s.group();
+keyboardGroup.attr({
+  transform: "translate(10, 55)"
+});
+
+Snap.load("svg/octave_keyboard.svg", onSVGLoaded);
+
+function onSVGLoaded(data) {
+  var fragment = data.selectAll("use");
+  // assign id's to all keys
+  for(var i = 0; i < whiteKeys.length; i++) {
+    fragment[i].attr({id: whiteKeys[i]});
+  }
+
+  for(i = whiteKeys.length; i < fragment.length; i++) {
+    fragment[i].attr({id: blackKeys[i - whiteKeys.length]});
+  }
+  keyboardGroup.append(data);
 }
 
 // establish the audio context
@@ -42,17 +98,29 @@ audioContext = new AudioContext();
 
 var playNote = function(e) {
   osc          = audioContext.createOscillator();
-  osc.type         = "sine";
+  osc.type         = oscType;
   osc.frequency.setValueAtTime(notes[e.target.id], audioContext.currentTime);
   osc.connect(audioContext.destination);
   osc.start(audioContext.currentTime);
   playing = true;
-  console.log(e.target.id);
 };
 
 var stopNote = function() {
   osc.stop();
 };
 
-s.mousedown(playNote);
-s.mouseup(stopNote);
+function changeOscillator() {
+  if(oscIndex < 3) {
+    oscIndex++;
+  } else {
+    oscIndex = 0;
+  }
+
+  oscType = oscillators[oscIndex];
+  btnText.attr({text: oscType});
+}
+
+btnCover.click(changeOscillator);
+
+keyboardGroup.mousedown(playNote);
+keyboardGroup.mouseup(stopNote);
